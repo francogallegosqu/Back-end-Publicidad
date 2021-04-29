fd281c41a69fe21051cb860627e2f1cdaae7d12e
francogallegosqu <francogallegos02@outlook.com>
2021-04-28 22:16:25 -0500

    first commit

---
+++ src/main/java/com/producto/publicidad/app/ProyectPublicidadApplication.java
@@ -0,0 +1,13 @@
+package com.producto.publicidad.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectPublicidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectPublicidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/producto/publicidad/app/ProyectPublicidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producto.publicidad.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectPublicidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
