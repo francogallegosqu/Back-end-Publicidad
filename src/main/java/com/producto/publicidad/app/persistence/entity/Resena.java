239affb3a8d394cdd1f08ffbf2834eb23605991a
Hector <hector.jesus.herrera@gmail.com>
2021-04-29 02:54:24 -0300

    union de tablas resena y publicaciones

--- src/main/java/com/producto/publicidad/app/persistence/entity/Resena.java
+++ src/main/java/com/producto/publicidad/app/persistence/entity/Resena.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +27,12 @@ public class Resena implements Serializable{
 		@ManyToOne
 		@JoinColumn(name="id_cliente")
 		private Cliente cliente;
+		
+		@OneToMany
+		@JoinColumn(name = "id_publicaciones")
+		private Publicaciones publicacion;
+
+		
 		
 		public Resena() {
 			
